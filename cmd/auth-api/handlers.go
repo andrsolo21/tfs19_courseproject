79aad008d61a850a7f00049f2a63ff45f23b2f7a
Unknown <andrsolo21@gmail.com>
2019-03-11 13:27:10 +0300

    main запуск

--- cmd/auth-api/handlers.go
+++ cmd/auth-api/handlers.go
@@ -1,4 +1,60 @@
 package main
 
-import "net/http"
+import (
+	"courseproject/internal/auth"
+	"courseproject/internal/user"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
 
+func signup(w http.ResponseWriter, r *http.Request){
+
+	var resp user.User
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "err %q\n", err, err.Error())
+	} else {
+		err = json.Unmarshal(body, &resp)
+		if err != nil {
+			fmt.Println(w, "can't unmarshal: ", err.Error())
+		}
+	}
+	resp.Created_at = time.Now()
+	resp.Updated_at = time.Now()
+
+	//fmt.Fprintf(w, "\nEmail:\n", resp.Email, "\nPassword:\n", resp.Password)
+	fmt.Fprintln(w,"id: ", resp.ID)
+	//fmt.Println("id: ", resp)
+	fmt.Fprintln(w,"fn: ", resp.First_name)
+	fmt.Fprintln(w,"ln: ", resp.Last_name)
+	fmt.Fprintln(w,"e: ", resp.Email)
+	fmt.Fprintln(w,"p: ", resp.Password)
+	fmt.Fprintln(w,"birth", resp.Birthday.String())
+	w.Write([]byte("\nGreetings 12345!"))
+}
+
+func signup2(w http.ResponseWriter, r *http.Request){
+	w.Header().Add("X-MY-LOCATION", "ALASKA")
+	w.Write([]byte("\nGreetings 12345!"))
+}
+
+func signin(w http.ResponseWriter, r *http.Request){
+
+	var resp auth.Logpas
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "err %q\n", err, err.Error())
+	} else {
+		err = json.Unmarshal(body, &resp)
+		if err != nil {
+			fmt.Println(w, "can't unmarshal: ", err.Error())
+		}
+	}
+
+	w.Write([]byte("\nGreetings 12345!"))
+}
---
+++ cmd/auth-api/services.go
@@ -0,0 +1,7 @@
+package main
+
+import "courseproject/internal/user"
+
+func AddUser(add user.User){
+
+}
